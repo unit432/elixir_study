390d20928bd143fd732dbbbfddbbc647811d8f07
Unit432 <cdong1@ualberta.ca>
2022-02-20 15:05:44 -0700

    format code

--- lib/sort.ex
+++ lib/sort.ex
@@ -1,7 +1,16 @@
 defmodule Sort do
+  @doc """
+  Merge Sort
+
+  ## Examples
+
+    iex> Sort.merge_sort([99, 3, 1, 18])
+    [1, 3, 18, 99]
+  """
   def merge_sort(list) when length(list) < 2, do: list
+
   def merge_sort(list) do
-    { left, right } = Enum.split(list, div(length(list), 2))
+    {left, right} = Enum.split(list, div(length(list), 2))
     :lists.merge(merge_sort(left), merge_sort(right))
   end
 end
